50cb01e72c7c41dcf96d66b961d46a33d33a8a97
askepen <aske@vhg26.dk>
2020-09-28 10:45:23 +0200

    Added simple baseline SVM

--- simple-model.py
+++ simple-model.py
@@ -4,6 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 from matplotlib import cm
 plt.style.use('ggplot')
@@ -15,17 +16,16 @@ y = data.iloc[:,-1]
 # Reduce dimensions
 X_pca = PCA(n_components=2).fit_transform(X)
 
-plt.scatter(X_pca[:,0], X_pca[:,1], c=y.values)
-plt.colorbar()
-plt.xlabel("PCA Axis 1")
-plt.ylabel("PCA Axis 2")
-plt.show()
+# plt.scatter(X_pca[:,0], X_pca[:,1], c=y.values)
+# plt.colorbar()
+# plt.xlabel("PCA Axis 1")
+# plt.ylabel("PCA Axis 2")
+# plt.show()
 
 kf = KFold(n_splits=10)
 kf.get_n_splits(X)
 
-scores = []
-best_score = np.inf
+best_score = {'k': 0, 'score': np.inf}
 
 for k in range(1,30)[::2]:
     for train_index, val_index in kf.split(X):
@@ -38,12 +38,27 @@ for k in range(1,30)[::2]:
 
         preds = model.predict(X_val)
         rmse = mean_squared_error(preds, y_val)
-        scores.append({
-            'k': k,
-            'RMSE': rmse,
-        })
-        if rmse < best_score:
-            best_score = rmse
-            print(k,rmse)
+        if rmse < best_score['score']:
+            best_score = {'k': k, 'score': rmse}
+
+print(best_score)
 
 
+# SVM approach
+
+model = SVC()
+
+best_score = np.inf
+
+for train_index, val_index in kf.split(X):
+    X_train, X_val = X_pca[train_index], X_pca[val_index]
+    y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_val)
+    rmse = mean_squared_error(preds, y_val)
+    if rmse < best_score:
+        best_score = rmse
+
+print(best_score)

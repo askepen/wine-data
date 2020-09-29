acb7180dae1dc8bbdf68523923d2599a3ae37886
askepen <aske@vhg26.dk>
2020-09-29 13:28:38 +0200

    Loading hyperparams from dict

--- deep-model.py
+++ deep-model.py
@@ -1,36 +1,34 @@
-# Copyright The PyTorch Lightning team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 from argparse import ArgumentParser
+from time import time
 
+import wandb
 import pandas as pd
-
-import torch
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning.loggers import WandbLogger
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 
-from time import time
+# Sweep parameters
+hyperparameter_defaults = dict(
+    batch_size = 32,
+    hidden_dim = 128,
+    learning_rate = 1e-3,
+    max_epochs = 30
+)
+
+wandb.init(config=hyperparameter_defaults)
+config = wandb.config
 
 
 class LitWineRegressor(pl.LightningModule):
-    def __init__(self, hidden_dim=128, learning_rate=1e-3):
+    def __init__(self, hparams):
         super().__init__()
-        self.save_hyperparameters()
-
-        self.l1 = torch.nn.Linear(12, self.hparams.hidden_dim)
-        self.l2 = torch.nn.Linear(self.hparams.hidden_dim, 1)
+        self.hidden_dim = hparams.hidden_dim # Needs to have some value before initialization
+        self.save_hyperparameters(hparams)
+        self.l1 = torch.nn.Linear(12, self.hidden_dim)
+        self.l2 = torch.nn.Linear(self.hidden_dim, 1)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
@@ -65,14 +63,6 @@ class LitWineRegressor(pl.LightningModule):
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
-    @staticmethod
-    def add_model_specific_args(parent_parser):
-        parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--hidden_dim', type=int, default=128)
-        parser.add_argument('--learning_rate', type=float, default=0.0001)
-        return parser
-
-
 class WineDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         """
@@ -99,18 +89,18 @@ class WineDataset(Dataset):
         return sample
 
 
-def cli_main():
+def cli_main(hparams):
     pl.seed_everything(1337)
 
     # ------------
     # args
     # ------------
     parser = ArgumentParser()
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--max-epochs', default=10, type=int)
-    #parser.add_argument('--row_log_interval', default=5, type=int)
+    parser.add_argument('--batch_size', default=hparams.batch_size, type=int)
+    parser.add_argument('--max-epochs', default=hparams.max_epochs, type=int)
     parser = pl.Trainer.add_argparse_args(parser)
-    parser = LitWineRegressor.add_model_specific_args(parser)
+    parser.add_argument('--learning_rate', default=hparams.learning_rate, type=int)
+    parser.add_argument('--hidden_dim', default=hparams.hidden_dim, type=int)
     args = parser.parse_args()
 
     # ------------
@@ -121,14 +111,14 @@ def cli_main():
     train_len = round(len(dataset)*0.8)
     wine_train, wine_val = random_split(dataset, [train_len, len(dataset)-train_len])
 
-    train_loader = DataLoader(wine_train, batch_size=args.batch_size)
-    val_loader = DataLoader(wine_val, batch_size=args.batch_size)
-    test_loader = DataLoader(wine_test, batch_size=args.batch_size)
+    train_loader = DataLoader(wine_train, batch_size=hparams.batch_size)
+    val_loader = DataLoader(wine_val, batch_size=hparams.batch_size)
+    test_loader = DataLoader(wine_test, batch_size=hparams.batch_size)
 
     # ------------
     # model
     # ------------
-    model = LitWineRegressor(args.hidden_dim, args.learning_rate)
+    model = LitWineRegressor(args)
 
     # ------------
     # logger
@@ -138,7 +128,8 @@ def cli_main():
     # ------------
     # training
     # ------------
-    trainer = pl.Trainer.from_argparse_args(args, logger=wandb_logger, row_log_interval=5)
+    print(args)
+    trainer = pl.Trainer.from_argparse_args(args,logger=wandb_logger, row_log_interval=5)
     trainer.fit(model, train_loader, val_loader)
 
     # ------------
@@ -149,4 +140,4 @@ def cli_main():
 
 
 if __name__ == '__main__':
-    cli_main()
+    cli_main(config)
